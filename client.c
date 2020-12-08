ef5887380cb38aed524ed919c063534d1abd66f3
Gnavyas <gnavyas@example.com>
2020-12-09 00:24:06 +0530

    Added support to transfer commands, execute and server decide OK/ERROR

--- client.c
+++ client.c
@@ -9,9 +9,87 @@
 
 #define BUFLEN 512 // buffer size
 #define OK "OK"
+#define ERROR "ERROR"
 
 void process_commands(int cliFd) {
 	// process server commands and execute them
+	char buffer[BUFLEN];
+	char command[5];
+	double d1;
+	double d2;
+	double resultDouble;
+	int randomChoice;
+	int resultInt;
+	int i1;
+	int i2;
+	
+	recv(cliFd, buffer, BUFLEN, 0);
+	fprintf(stdout, "** Server sent following command -- %s", buffer);
+	
+	if(strstr(buffer, "fadd") != NULL) {
+		sscanf(buffer, "%s %lf %lf\n", command, &d1, &d2);
+		resultDouble = d1 + d2;
+		sprintf(buffer, "%8.8g", resultDouble);
+		fprintf(stdout, "** Sending answer %8.8g to server\n", resultDouble);
+		send(cliFd, buffer, BUFLEN, 0);
+	}
+	else if(strstr(buffer, "fdiv") != NULL) {
+		sscanf(buffer, "%s %lf %lf\n", command, &d1, &d2);	
+		resultDouble = d1 / d2;
+		sprintf(buffer, "%8.8g", resultDouble);
+		fprintf(stdout, "** Sending answer %8.8g to server\n", resultDouble);
+		send(cliFd, buffer, BUFLEN, 0);
+	}
+	else if(strstr(buffer, "fmul") != NULL) {
+		sscanf(buffer, "%s %lf %lf\n", command, &d1, &d2);
+		resultDouble = d1 * d2;
+		sprintf(buffer, "%8.8g", resultDouble);
+		fprintf(stdout, "** Sending answer %8.8g to server\n", resultDouble);
+		send(cliFd, buffer, BUFLEN, 0);
+	}
+	else if(strstr(buffer, "fsub") != NULL) {
+		sscanf(buffer, "%s %lf %lf\n", command, &d1, &d2);	
+		resultDouble = d1 - d2;
+		sprintf(buffer, "%8.8g", resultDouble);
+		fprintf(stdout, "** Sending answer %8.8g to server\n", resultDouble);
+		send(cliFd, buffer, BUFLEN, 0);
+	}
+	else if(strstr(buffer, "add") != NULL) {
+		sscanf(buffer, "%s %d %d\n", command, &i1, &i2);
+		resultInt = i1 + i2;
+		sprintf(buffer, "%d", resultInt);
+		fprintf(stdout, "** Sending answer %d to server\n", resultInt);
+		send(cliFd, buffer, BUFLEN, 0);
+	}
+	else if(strstr(buffer, "div") != NULL) {
+		sscanf(buffer, "%s %d %d\n", command, &i1, &i2);	
+		resultInt = i1 / i2;
+		sprintf(buffer, "%d", resultInt);
+		fprintf(stdout, "** Sending answer %d to server\n", resultInt);
+		send(cliFd, buffer, BUFLEN, 0);
+	}
+	else if(strstr(buffer, "mul") != NULL) {
+		sscanf(buffer, "%s %d %d\n", command, &i1, &i2);	
+		resultInt = i1 * i2;
+		sprintf(buffer, "%d", resultInt);
+		fprintf(stdout, "** Sending answer %d to server\n", resultInt);
+		send(cliFd, buffer, BUFLEN, 0);
+	}
+	else if(strstr(buffer, "sub") != NULL) {
+		sscanf(buffer, "%s %d %d\n", command, &i1, &i2);
+		resultInt = i1 - i2;
+		sprintf(buffer, "%d", resultInt);
+		fprintf(stdout, "** Sending answer %d to server\n", resultInt);
+		send(cliFd, buffer, BUFLEN, 0);
+	}
+	else { // invalid operation requested
+		send(cliFd, "0", 2, 0);
+	}
+	recv(cliFd, buffer, sizeof(buffer), 0);
+	if(strcmp(buffer, OK) == 0)
+		fprintf(stdout, "%% Server sent %s\n", OK);
+	else
+		fprintf(stdout, "%% Server sent %s\n", ERROR);
 }
 
 int main(int argc, char *argv[]) {
@@ -32,11 +110,13 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "%% Couldn't connect to server. Exiting\n"); 
 		exit(0); 
 	}
+	fprintf(stdout, "%% Connected to the server\n");
 	// communicate
 	recv(cliFd, buffer, BUFLEN, 0);
 	fprintf(stdout, "** SERVER sent %s\n", buffer);
-	send(clifd, OK, sizeof(OK), 0);
+	send(cliFd, OK, sizeof(OK), 0);
 	process_commands(cliFd);
+	fprintf(stdout, "** Processing server requests done. Closing connection\n");
 	close(cliFd);
 	return 0;
 }
--- server.c
+++ server.c
@@ -5,17 +5,88 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #define PROTOCOLS "TEXT TCP 1.0" // constant msg
+#define MAXADDRANGE 100000 // Can change this to (maxsize of int / 2)
+#define BUFLEN 512 // buffer size
+#define OK "OK"
+#define ERROR "ERROR"
+#define IPLEN 16
 
 void send_commands(int connectionFd) {
 	// get random commands and send it to client to execute
-	// get output
+	char buffer[BUFLEN];
+	char calcInt[4][10] = {"add", "div", "mul", "sub"};
+	char calcDouble[4][10] = {"fadd", "fdiv", "fmul", "fsub"};
+	char tempBuf[50];
+	double d1;
+	double d2;
+	double resultDouble;
+	double p = 0.00001; // precision checking
+	int randomChoice;
+	int resultInt;
+	int i1;
+	int i2;
+	
+	srand(time(0));
+	randomChoice = rand() % 2; // generates 0 or 1
+	if(randomChoice) {
+		// let's send int nums
+		i1 = rand() % MAXADDRANGE;
+		i2 = rand() % MAXADDRANGE;
+		sprintf(tempBuf, "%d %d", i1, i2);
+		randomChoice = rand() % 4;
+		sprintf(buffer, "%s %s\n", calcInt[randomChoice], tempBuf);
+		fprintf(stdout, "%% Sending command to execute -- %s", buffer);
+		send(connectionFd, buffer, BUFLEN, 0);
+		recv(connectionFd, buffer, BUFLEN, 0);
+		fprintf(stdout, "%% Client returned the value %s\n", buffer);
+		sscanf(buffer, "%d", &resultInt);
+		switch(randomChoice) {
+			case 0: (resultInt == i1 + i2) ? send(connectionFd, OK, sizeof(OK), 0) : send(connectionFd, ERROR, sizeof(ERROR), 0);
+				break;
+			case 1: (resultInt == i1 / i2) ? send(connectionFd, OK, sizeof(OK), 0) : send(connectionFd, ERROR, sizeof(ERROR), 0);
+				break;
+			case 2: (resultInt == i1 * i2) ? send(connectionFd, OK, sizeof(OK), 0) : send(connectionFd, ERROR, sizeof(ERROR), 0);
+				break;
+			case 3: (resultInt == i1 - i2) ? send(connectionFd, OK, sizeof(OK), 0) : send(connectionFd, ERROR, sizeof(ERROR), 0);
+				break;
+		}
+	}
+	else {
+		// let's send double nums
+		d1 = (float) rand() / RAND_MAX;
+		d2 = (float) rand() / RAND_MAX;
+		sprintf(tempBuf, "%8.8g %8.8g", d1, d2);
+		sscanf(tempBuf, "%lf %lf", &d1, &d2);
+		randomChoice = rand() % 4;
+		sprintf(buffer, "%s %s\n", calcDouble[randomChoice], tempBuf);
+		fprintf(stdout, "%% Sending command to execute -- %s", buffer);
+		send(connectionFd, buffer, BUFLEN, 0);
+		recv(connectionFd, buffer, BUFLEN, 0);
+		fprintf(stdout, "%% Client returned the value %s\n", buffer);
+		sscanf(buffer, "%d", &resultInt);
+		switch(randomChoice) {
+			case 0: ((resultDouble - p < d1 + d2) && (resultDouble + p > d1 + d2)) ? send(connectionFd, OK, sizeof(OK), 0) : send(connectionFd, ERROR, sizeof(ERROR), 0);
+				break;
+			case 1: ((resultDouble - p < d1 / d2) && (resultDouble + p > d1 + d2)) ? send(connectionFd, OK, sizeof(OK), 0) : send(connectionFd, ERROR, sizeof(ERROR), 0);
+				break;
+			case 2: ((resultDouble - p < d1 * d2) && (resultDouble + p > d1 + d2)) ? send(connectionFd, OK, sizeof(OK), 0) : send(connectionFd, ERROR, sizeof(ERROR), 0);
+				break;
+			case 3: ((resultDouble - p < d1 - d2) && (resultDouble + p > d1 + d2)) ? send(connectionFd, OK, sizeof(OK), 0) : send(connectionFd, ERROR, sizeof(ERROR), 0);
+				break;
+		}
+	}
+
 }
 
 int main(int argc, char *argv[]) {
 	struct sockaddr_in serverAddr, client;
 	int servFd, connectionFd, length;
+	char buffer[BUFLEN];
+	char ip[IPLEN];
+		
 	
 	servFd = socket(AF_INET, SOCK_STREAM, 0);
 	if(servFd == -1) {
@@ -37,21 +108,23 @@ int main(int argc, char *argv[]) {
 	}
 	
 	length = sizeof(client);
-	connectionFd = accept(servFd, (struct sockaddr *)&client, &length);
-	// communicate
-	fprintf(stdout, "%% Connected to the client\n");
-	send(connectionFd, PROTOCOLS, sizeof(PROTOCOLS), 0);
-	recv(cliFd, buffer, BUFLEN, 0);
-	fprintf(stdout, "%% SERVER sent %s\n", buffer);
+	while(1) {
+		fprintf(stdout, "%% Waiting for the new client to connect\n");
+		connectionFd = accept(servFd, (struct sockaddr *)&client, &length);
+		fprintf(stdout, "%% New Client has connected\n");
+		// communicate
+		fprintf(stdout, "%% Connected to the client\n");
+		send(connectionFd, PROTOCOLS, sizeof(PROTOCOLS), 0);
+		recv(connectionFd, buffer, BUFLEN, 0);
+		fprintf(stdout, "%% SERVER sent %s\n", buffer);
 
-	if(strcmp(buffer, OK) == 0) {
-		send_commands(connectionFd);
-	}
-	else {
-		fprintf(stderr, "%% Closing client connection");
+		if(strcmp(buffer, OK) == 0) {
+			send_commands(connectionFd);
+		}
+		fprintf(stderr, "%% Closing client connection\n\n");
 		// close connection here
+		close(connectionFd);
 	}
-
 	close(servFd);
 	return 0;
 }
